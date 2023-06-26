c3f2a3533cae03d88fe50096732b2b9e88a8d3a8
Luciano Salotto <lsalotto@gmail.com>
2023-06-26 20:00:44 -0300

    Agrega test para Materia Controller

--- src/main/java/ar/edu/utn/frbb/tup/model/Materia.java
+++ src/main/java/ar/edu/utn/frbb/tup/model/Materia.java
@@ -2,6 +2,7 @@ package ar.edu.utn.frbb.tup.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Materia {
 
@@ -77,5 +78,18 @@ public class Materia {
 
     public void setMateriaId(int materiaId) {
         this.materiaId = materiaId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Materia materia = (Materia) o;
+        return materiaId == materia.materiaId && anio == materia.anio && cuatrimestre == materia.cuatrimestre && Objects.equals(nombre, materia.nombre) && Objects.equals(profesor, materia.profesor) && Objects.equals(correlatividades, materia.correlatividades);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(materiaId, nombre, anio, cuatrimestre, profesor, correlatividades);
     }
 }
--- src/test/java/ar/edu/utn/frbb/tup/controller/MateriaControllerTest.java
+++ src/test/java/ar/edu/utn/frbb/tup/controller/MateriaControllerTest.java
@@ -1,4 +1,72 @@
 package ar.edu.utn.frbb.tup.controller;
 
+import ar.edu.utn.frbb.tup.App;
+import ar.edu.utn.frbb.tup.business.MateriaService;
+import ar.edu.utn.frbb.tup.model.Materia;
+import ar.edu.utn.frbb.tup.model.dto.MateriaDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@ExtendWith(SpringExtension.class)
 public class MateriaControllerTest {
+
+    @InjectMocks
+    MateriaController materiaController;
+
+    @Mock
+    MateriaService materiaService;
+
+    MockMvc mockMvc;
+
+    private static ObjectMapper mapper = new ObjectMapper();
+
+    @BeforeEach
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(materiaController).build();
+    }
+
+    @Test
+    public void crearMateriaTest() throws Exception {
+
+        Mockito.when(materiaService.crearMateria(any(MateriaDto.class))).thenReturn(new Materia());
+        MateriaDto materiaDto = new MateriaDto();
+        materiaDto.setAnio(1);
+        materiaDto.setCuatrimestre(2);
+        materiaDto.setNombre("Laboratorio II");
+        materiaDto.setProfesorId(345);
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/materia/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(materiaDto))
+                .accept(MediaType.APPLICATION_JSON)).andExpect(status().is2xxSuccessful())
+                .andReturn();
+
+
+
+        Assertions.assertEquals(new Materia(), mapper.readValue(result.getResponse().getContentAsString(), Materia.class));
+    }
+
+
 }
